89b1ced7f881a5b1d2230954d48d4dfa9a9d3560
PePe <svobodapetr803@gmail.com>
2022-11-29 21:25:12 +0100

    db connection, create models

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Course = require('../models/courseModel');
+
 // @desc Get courses
 // @route GET /api/coourses
 // @access Private
 const getCourse = asyncHandler(async (req, res) => {
-    res.status(200).json({"message": "Get course"})
+    const courses = await Course.find()
+
+    res.status(200).json(courses)
 })
 
 // @desc Create courses
@@ -15,21 +19,46 @@ const setCourse = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Please add text")
     }
-    res.status(200).json({"message": "Set course"})
+
+    const course = await Course.create({
+        text: req.body.text,
+    })
+
+    res.status(200).json(course)
 })
 
 // @desc Update courses
 // @route PUT /api/coourses/:id
 // @access Private
 const updateCourse = asyncHandler(async (req, res) => {
-    res.status(200).json({"message": `Update course ${req.params.id}`})
+    const course = await Course.findById(req.params.id)
+
+    if(!course) {
+        res.status(400)
+        throw new Error("Course not find")
+    }
+
+    const updatedCourse = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedCourse)
 })
 
 // @desc Delete courses
 // @route DELETE /api/coourses/:id
 // @access Private
 const deleteCourse = asyncHandler(async (req, res) => {
-    res.status(200).json({"message": `Delete course ${req.params.id}`})
+    const course = await Course.findById(req.params.id)
+
+    if(!course) {
+        res.status(400)
+        throw new Error("Course not find")
+    }
+
+    await course.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
