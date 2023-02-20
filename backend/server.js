1eef044c3b1018c6d6c5af8996acf8572ac65bab
PePe <svobodapetr803@gmail.com>
2023-02-20 23:38:38 +0100

    fix

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ app.use("/api/attendances", require("./routes/attendanceRoutes"))
 app.use("/api/permissions", require("./routes/permissionRoutes"))
 
 if(process.env.NODE_ENV == "production") {
-    app.use(express.static(path.join(__dirname, "../build")))
+    app.use(express.static(path.join(__dirname, "../")))
 
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "build", "index.html")))
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "index.html")))
 } else {
     app.get("/", (req, res) => res.send("Please set to production"))
 }
