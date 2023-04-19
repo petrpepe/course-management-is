abefd56ffe9f4847e5cbf1e9384f7c38a4ad2712
PePe <svobodapetr803@gmail.com>
2023-04-19 23:32:23 +0200

    email fix for render

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -1,10 +1,17 @@
 const nodemailer = require("nodemailer")
-const Transport = require("nodemailer-sendinblue-transport")
+const Transport = require("../emailConfig/transport")
 
 const sendEmail = async (sent_from, send_to, reply_to = "", subject, content) => {
-  const transporter = nodemailer.createTransport(
-    new Transport({apiKey: process.env.EMAIL_API_KEY})
-  )
+  const transporter = nodemailer.createTransport(new Transport({apiKey: process.env.EMAIL_API_KEY}));
+
+  /*
+    service: 'gmail',
+    auth: {
+      user: 'svobodapetr803@gmail.com',
+      pass: 'https://myaccount.google.com/u/0/apppasswords?rapt=AEjHL4PPHz7ZWkUfa2AQZK-rE7WvUyrTdaEcjm05WCiGAVhnwlM24h2nRbnq1oWG5dBZLNH_vJ2q5kCjjn3cLjCxPE7pDasqkw'
+    }
+  */
+  //new Transport({apiKey: process.env.EMAIL_API_KEY})
 
   const options = {
     from: sent_from,
