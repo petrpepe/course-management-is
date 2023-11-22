9d2b7c7f0cd5a1f6951b738fb89a5ca67a25a938
Petr Svoboda <svobodapetr803@gmail.com>
2023-11-22 15:24:03 +0100

    Tiny fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://sis-cr-fe.onrender.com"],
+  origin: [
+    "http://localhost:3000",
+    "https://sis-cr-fe.onrender.com",
+    "https://sis-cr.onrender.com",
+  ],
   methods: "GET,POST,PUT,DELETE",
   optionsSuccessStatus: 200,
 };
