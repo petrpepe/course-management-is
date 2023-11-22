183893306bb4f14bcc478e81a640d3a92c90c4d6
Petr Svoboda <svobodapetr803@gmail.com>
2023-11-22 11:18:58 +0100

    Fixes, html meta icons

--- frontend/src/pages/classes/Classes.jsx
+++ frontend/src/pages/classes/Classes.jsx
@@ -3,6 +3,10 @@ import {
   getClasses,
   reset as resetClasses,
 } from "../../features/classes/classSlice";
+import {
+  getCourses,
+  reset as resetCourses,
+} from "../../features/courses/courseSlice";
 import CustomCard from "../../components/CustomCard";
 import Search from "../../components/Search";
 import { Status } from "../../features/Status";
@@ -11,9 +15,18 @@ import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
 import ActionPermLink from "../../components/form/ActionPermLink";
 import Grid from "@mui/material/Unstable_Grid2";
+import Button from "@mui/material/Button";
+import { Link as ReactLink } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 function Classes() {
   const { classes, status } = useGetData("classes", getClasses, resetClasses);
+  const { courses, status: courseStatus } = useGetData(
+    "courses",
+    getCourses,
+    resetCourses,
+    { ids: classes.map((c) => c.course) }
+  );
 
   return (
     <>
@@ -25,22 +38,34 @@ function Classes() {
         perm="classCreate"
       />
 
-      {status === Status.Loading ? (
+      {status === Status.Loading || courseStatus === Status.Loading ? (
         <CircularProgress />
       ) : classes.length > 0 ? (
-        <Grid container spacing={3} justifyContent="space-evenly">
-          {classes.map((classVar) => (
-            <Grid key={classVar._id}>
-              <CustomCard
-                data={classVar}
-                link="/classes/"
-                deleteAction={deleteClass}
-                deletePerm="classDelete"
-                editPerm="classUpdate"
-              />
+        courses.map((c) => (
+          <Box
+            sx={{ border: 1, px: 2, pb: 2, pt: 1, borderRadius: 1 }}
+            key={c._id}>
+            <Button
+              component={ReactLink}
+              to={"/courses/" + c._id}
+              sx={{ mb: 1 }}>
+              {c.title}
+            </Button>
+            <Grid container spacing={3} justifyContent="space-evenly">
+              {classes.map((classVar) => (
+                <Grid key={classVar._id}>
+                  <CustomCard
+                    data={classVar}
+                    link="/classes/"
+                    deleteAction={deleteClass}
+                    deletePerm="classDelete"
+                    editPerm="classUpdate"
+                  />
+                </Grid>
+              ))}
             </Grid>
-          ))}
-        </Grid>
+          </Box>
+        ))
       ) : (
         <Typography variant="h3">
           You are not enrolled in any classes
