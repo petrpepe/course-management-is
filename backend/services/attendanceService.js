8dcaf5c5e73c00b6b5ac98b72d6dc2afd9e2044f
Petr Svoboda <svobodapetr803@gmail.com>
2023-12-11 15:47:18 +0100

    Clean up

--- backend/services/attendanceService.js
+++ backend/services/attendanceService.js
@@ -11,17 +11,18 @@ const getAttendanceParams = asyncHandler(async (req, res, next) => {
     const ids = Array.isArray(id)
       ? id.map((id) => new mongoose.Types.ObjectId(id))
       : id.split(",").map((id) => new mongoose.Types.ObjectId(id));
-    const enrollments = await Enrollment.find({ students: { $in: ids } });
+    const enrollments = await Enrollment.find({
+      students: { $in: ids },
+    }).select("classId");
     const timetables = await Timetable.find({
       classId: { $in: ids.concat(enrollments.map((e) => e.classId)) },
-    });
-    arg = {
-      $or: [
-        { timetableId: { $in: timetables.map((t) => t._id).concat(ids) } },
-        { userId: { $in: ids } },
-        { _id: { $in: ids } },
-      ],
-    };
+    }).select("_id");
+
+    arg.$or = [
+      { timetableId: { $in: timetables.map((t) => t._id).concat(ids) } },
+      { userId: { $in: ids } },
+      { _id: { $in: ids } },
+    ];
   }
 
   if (startDatetime && endDatetime) {
